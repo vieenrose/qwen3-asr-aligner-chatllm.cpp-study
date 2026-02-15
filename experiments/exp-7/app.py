799285207cd210246f1a4913151d6aca8839307b
luigi <louis_liu@pesi.com.tw>
2026-02-15 11:46:34 +0800

    fix: Add allowed_paths for Gradio to serve sample audio files
    - Add /app/samples to allowed_paths in launch()
    - Fix InvalidPathError when loading sample audio

--- experiments/exp-7/app.py
+++ experiments/exp-7/app.py
@@ -284,4 +284,4 @@ with gr.Blocks(
 
 
 if __name__ == '__main__':
-    app.launch(server_name='0.0.0.0', server_port=7860)
+    app.launch(server_name='0.0.0.0', server_port=7860, allowed_paths=['/app/samples'])
